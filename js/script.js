17a526c7be431fb98e24389ab88e77b57862e954
ta-design2021 <91456029+ta-design2021@users.noreply.github.com>
2022-06-07 16:26:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function fadeAnime(){
     fadeAnime();/* アニメーション用の関数を呼ぶ*/
   });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
-  /*=================================================
+/*=================================================
 SERVICEセクションの順番にふわっとfadeUp
 ===================================================*/
 var effect_btm = 300; // 画面下からどの位置でフェードさせるか(px)
@@ -133,6 +133,37 @@ $(window).on('scroll load', function(){
     $('body,html').stop().animate({scrollTop:position}, 500); // スクロール速度ミリ秒
     return false; // #付与なし、付与したい場合は、true
   });
+
+  /*=================================================
+  slider
+  ===================================================*/
+
+  $('.slider').slick({
+		arrows: false,//左右の矢印はなし
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+		speed: 6900,//スライドのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+		pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+		cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+		slidesToShow: 5,//スライドを画面に5枚見せる
+		slidesToScroll: 1,//1回のスライドで動かす要素数
+		responsive: [
+			{
+			breakpoint: 768,//モニターの横幅が768px以下の見せ方
+			settings: {
+				slidesToShow: 3,//スライドを画面に3枚見せる
+			}
+		},
+		{
+			breakpoint: 426,//モニターの横幅が426px以下の見せ方
+			settings: {
+				slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+			}
+		}
+	]
+	});
 
 });
 
